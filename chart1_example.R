b2eb9885ac28de946e3e38a99018afef7f15d6c4
reollei <leila.peitsch@gmail.com>
2023-08-05 09:35:42 -0700

    Finished summary.R and chart1-example.R

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,35 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
-
+# Load packages
 library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+
+# Clean data
+chart_df <- jail_pop %>% 
+  # Focus on Georgia as they have comparable black and white populations
+  filter(state == "GA", year > 2000) %>% 
+  mutate(other_jail_pop = aapi_jail_pop + native_jail_pop + latinx_jail_pop + other_race_jail_pop) %>% 
+  # Choose columns of white, black, aapi, and native jail pop.
+  select(year, white_jail_pop, black_jail_pop, other_jail_pop) %>%
+  # Dropping NA values as they can affect the graph
+  drop_na()
+
+# Sum the jail populations from all counties in GA
+chart_df <- chart_df %>% 
+  group_by(year) %>% 
+  summarise(across(c(white_jail_pop, black_jail_pop, other_jail_pop), sum))
+
+# Combine columns into one column (groups)
+chart_df <- chart_df %>% 
+  pivot_longer(cols=c('white_jail_pop', 'black_jail_pop', 
+                      'other_jail_pop'),
+                                      names_to='race',
+                                      values_to='pop')
+
+# Plot data
+ggplot(data = chart_df, aes(x = year, y = pop, group = race)) + 
+  geom_line(aes(color = race), linewidth = 0.7) + 
+  xlab("Year") +
+  ylab("Jail Population") +
+  ggtitle("Jail Populations in Georgia") +
+  theme(plot.title = element_text(hjust=0.5)) +
+  scale_color_manual(name = "Race", labels = c("Black", "Other", "White"), 
+                     values = c('firebrick', 'cornflowerblue', 'darkseagreen4'))
+
---
+++ summary.R
@@ -0,0 +1,80 @@
+# Load packages
+library(tidyverse)
+library(dplyr)
+
+# Load datasets
+prison_pop <- read.csv('https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-pop.csv?raw=true')
+jail_pop <- read.csv('https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-jail-pop.csv?raw=true')
+pop_rate_100k <- read.csv('https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-jail-rates.csv?raw=true')
+pop_rate_100k_in_90s <- read.csv('https://github.com/melaniewalsh/Neat-Datasets/blob/main/us-prison-jail-rates-1990.csv?raw=true')
+pop_rate_100k_in_90s_WA <- read.csv('https://github.com/melaniewalsh/Neat-Datasets/raw/main/us-prison-jail-rates-1990-WA.csv')
+
+# Note:
+#   Jail holds people awaiting trial/sentencing
+#   Prisons house people who have been convicted of a crime
+
+# Calculating 5 summary values
+
+# The state with the largest jail population within urban communities
+# from years 2000-2018
+high_urban_state_jail_pop <- jail_pop %>% 
+  filter(year > 2000, urbanicity == "urban") %>% 
+  filter(total_jail_pop == max(total_jail_pop, na.rm = TRUE)) %>% 
+  pull(state)
+
+# Convert abbreviation to state name
+high_urban_state_jail_pop <- state.name[match(high_urban_state_jail_pop, state.abb)]
+
+# The state with the largest jail population within rural communities
+# from years 2000-2018
+high_rural_state_jail_pop <- jail_pop %>% 
+  filter(year > 2000, urbanicity == "rural") %>% 
+  filter(total_jail_pop == max(total_jail_pop, na.rm = TRUE)) %>% 
+  pull(state)
+
+# Convert abbreviation to state name
+high_rural_state_jail_pop <- state.name[match(high_rural_state_jail_pop, state.abb)]
+
+# The highest reported black and white jail population within the 2000s in Texas
+high_black_jail_pop <- jail_pop %>% 
+  filter(year > 2000, state == "TX") %>% 
+  filter(black_jail_pop == max(black_jail_pop, na.rm = TRUE)) %>% 
+  pull(black_jail_pop)
+
+high_white_jail_pop <- jail_pop %>% 
+  filter(year > 2000, state == "TX") %>% 
+  filter(white_jail_pop == max(white_jail_pop, na.rm = TRUE)) %>% 
+  pull(white_jail_pop)
+
+# The state with largest ratio between black and white populations held in jail
+# during 2015 and what the ratio is (B / W)
+high_bw_ratio_state <- jail_pop %>% 
+  filter(year == 2015) %>% 
+  mutate(bw_ratio = black_jail_pop / white_jail_pop) %>% 
+  filter(!is.infinite(bw_ratio)) %>% 
+  filter(bw_ratio == max(bw_ratio, na.rm = TRUE)) %>% 
+  pull(state)
+
+# Convert abbreviation to state name
+high_bw_ratio_state <- state.name[match(high_bw_ratio_state, state.abb)]
+
+high_bw_ratio <- jail_pop %>% 
+  filter(year == 2015) %>% 
+  select(state, black_jail_pop, white_jail_pop) %>% 
+  mutate(bw_ratio = black_jail_pop / white_jail_pop) %>% 
+  filter(bw_ratio == max(bw_ratio, na.rm = TRUE)) %>% 
+  pull(bw_ratio)
+
+# The state with largest juvenile jail population (male and female)
+# within the 2000s
+high_juvenile_pop <- jail_pop %>% 
+  filter(year > 2000) %>% 
+  select(state, female_juvenile_jail_pop, male_juvenile_jail_pop) %>% 
+  mutate(total_juvenile_pop = female_juvenile_jail_pop + 
+           male_juvenile_jail_pop) %>%
+  filter(total_juvenile_pop == max(total_juvenile_pop, na.rm = TRUE)) %>% 
+  pull(state)
+
+# Convert abbreviation to state name
+high_juvenile_pop <- state.name[match(high_juvenile_pop, state.abb)]
+
